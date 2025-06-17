b28c4e9da60c582431ab825593c4a7dc6cf888d0
mendozasho <shomendoza@gmail.com>
2025-06-16 21:15:27 -0700

    Adding template for sqa db setup

--- dirlin/db/queries/quickstart.py
+++ dirlin/db/queries/quickstart.py
@@ -1,9 +1,15 @@
+from typing import TypeVar
+
+import pandas as pd
 from pydantic import BaseModel
 from sqlalchemy import select, update
 from sqlalchemy.orm import DeclarativeBase
 
 from dirlin.db.queries.query import Query
 from dirlin.db.setup import SqlSetup
+
+
+AnyBaseModelSub = TypeVar('AnyBaseModelSub', bound=BaseModel)
 
 
 class CreateOrUpdateRecord(Query):
@@ -49,15 +55,28 @@ class ReadRecordWithTransactionID(Query):
             table: type[DeclarativeBase],
             model: type[BaseModel],
             table_id_field: str,
-            id_lookup: str | None = None,
-    ) -> type[BaseModel]:
+            id_lookup: str,
+    ) -> AnyBaseModelSub:
+        with self.setup.session.begin() as sesh:
+            query = sesh.execute(
+                select(table).where(getattr(table, table_id_field) == id_lookup)  # type:ignore
+            )
+            return model.model_validate(query.scalar_one())
+
+
+class ReadTable(Query):
+    def __init__(self, setup: SqlSetup):
+        """reads the entire table in a Database and returns it as a Dataframe
+        """
+        self.setup = setup
+
+    def execute(
+            self,
+            table: type[DeclarativeBase],
+    ):
         with self.setup.session.begin() as sesh:
             query = sesh.execute(select(table))
-            if id_lookup is not None:
-                query = sesh.execute(
-                    select(table).where(getattr(table, table_id_field) == id_lookup)  # type:ignore
-                )
-            return model.model_validate(query.scalar_one())  # type:ignore
+        return pd.DataFrame(query.fetchall(), columns=list(query.keys()))
 
 
-__all__ = ['CreateOrUpdateRecord', 'ReadRecordWithTransactionID']
+__all__ = ['CreateOrUpdateRecord', 'ReadRecordWithTransactionID', 'ReadTable']
